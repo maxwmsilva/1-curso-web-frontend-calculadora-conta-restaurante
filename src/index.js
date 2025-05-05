6744f4fb7d6e10c03e1e538de8d17968e8bccb0c
Max Willian Marcelino da Silva <max-will-94@hotmail.com>
2025-05-05 20:28:44 -0300

    Add: Complet challenge using if and else

--- src/index.js
+++ src/index.js
@@ -5,13 +5,25 @@ const readline = require('readline-sync');
 const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa: ');
 
 // Capturando o valor total da conta
-const valorTotal = readline.questionFloat('Digite o valor total da conta: ');
+  const valorTotal = readline.questionFloat('Digite o valor total da conta: ');
 
 // Capturando o método de pagamento
-const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)? ');
+const metodoDePagamento  = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)? ');
 
 
-//Façã sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
+//Faça sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
+
+if (metodoDePagamento === 'PIX' || metodoDePagamento === 'dinheiro') {
+  let valorComDesconto = valorTotal - (valorTotal * 0.10);
+  
+    console.log(valorTotal, metodoDePagamento, numeroPessoas);
+  console.log('O valor da conta com desconto  é: R$:', valorComDesconto.toFixed(2), 'e o valor por pessoa é R$:', (valorComDesconto / numeroPessoas).toFixed(2));
+}
+else {
+  let valorComDesconto = valorTotal;
+  console.log(valorTotal, metodoDePagamento, numeroPessoas);
+  console.log('Como método é catão,  o valor da conta é: R$:', valorTotal.toFixed(2), 'e o valor por pessoa é R$:', (valorTotal / numeroPessoas).toFixed(2));
+}
 
 
-// Exibindo os resultados
+
